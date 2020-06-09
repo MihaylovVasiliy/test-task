d832b9fbd76f91f69977d8e5b0827589aa3de12e
Vasiliy-home <basen3012@gmail.com>
2020-06-09 06:26:05 +0400

    Updates

---
+++ precache-manifest.09bbab76b0971e0e5f1e7a99820321ae.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad093aeea7dc74ee4252326249ee4d71",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "8a23938920596b4f5ed5",
+    "url": "/task/static/css/main.62fec8d9.chunk.css"
+  },
+  {
+    "revision": "008618b0da1c33fa392e",
+    "url": "/task/static/js/2.31ea276b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/task/static/js/2.31ea276b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a23938920596b4f5ed5",
+    "url": "/task/static/js/main.5721dfce.chunk.js"
+  },
+  {
+    "revision": "bf65eef458843474f1c5",
+    "url": "/task/static/js/runtime-main.b0c76932.js"
+  },
+  {
+    "revision": "1ca60a7b26683cd6eedb4369792a1199",
+    "url": "/task/static/media/1-channel.1ca60a7b.svg"
+  },
+  {
+    "revision": "b66c7e3eabf09da2d0c966e3d61daa1c",
+    "url": "/task/static/media/2x2-channel.b66c7e3e.svg"
+  },
+  {
+    "revision": "c87313aa86b7caa31a9a0accaa584970",
+    "url": "/task/static/media/Rubik-Medium.c87313aa.ttf"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/task/static/media/Rubik-Regular.b3d0902b.ttf"
+  },
+  {
+    "revision": "4333f4661b735f3fb1269eff5e89bba8",
+    "url": "/task/static/media/amedia-premium-channel.4333f466.svg"
+  },
+  {
+    "revision": "efc916ff7202064679019469341d0720",
+    "url": "/task/static/media/htc-cs-logo.efc916ff.svg"
+  },
+  {
+    "revision": "2e3245d47c0ecf9f6d0f30bab15fda70",
+    "url": "/task/static/media/logo.2e3245d4.svg"
+  },
+  {
+    "revision": "7cedf79909ab02691a628f14dac57cab",
+    "url": "/task/static/media/newProduct-batman.7cedf799.png"
+  },
+  {
+    "revision": "e380a79e1fbb6afb14fc08e4e969aa4b",
+    "url": "/task/static/media/newProduct-hollywood.e380a79e.png"
+  },
+  {
+    "revision": "5e574b3f36b8b144975c7d29797c17c0",
+    "url": "/task/static/media/newProduct-kitty.5e574b3f.png"
+  },
+  {
+    "revision": "4692ec1072b0ae98f2c59678b8d7091c",
+    "url": "/task/static/media/newProduct-strip.4692ec10.png"
+  },
+  {
+    "revision": "5c4d8347d85163feb20fbd44532f83be",
+    "url": "/task/static/media/rbk-channel.5c4d8347.svg"
+  }
+]);
